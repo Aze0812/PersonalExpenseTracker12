81c87c77bce00c9fe33ee581cebadfa967fe8e65
Michael Grace <mhgracek09@gmail.com>
2025-05-30 04:42:05 +0800

    Add files via upload
    expenses, goal, subscription

---
+++ PersonalExpenseTracker13/Personal Expense Tracker/Personal Expense Tracker/ExpenseTracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Expense_Tracker
+{
+    public partial class ExpenseTracker: Form
+    {
+        public ExpenseTracker()
+        {
+            InitializeComponent();
+        }
+
+        private void ExpenseTracker_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form Tracker = new Tracker();
+            Tracker.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form goalTracker = new GoalTracker();
+            goalTracker.Show();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form subtracker = new SubscriptionTracker();
+            subtracker.Show();
+        }
+    }
+}
