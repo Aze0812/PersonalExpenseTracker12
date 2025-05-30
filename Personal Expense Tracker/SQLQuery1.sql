f28f40696a0f6530e0e31e02f1ac4bb46e397f4f
Jose Miguel Tolentino <85689172+sirmoonman@users.noreply.github.com>
2025-05-30 14:39:06 +0800

    Add files via upload
    FR7.1: Display transaction amounts, dates, categories, and payment
    methods. - Done
    FR 7.2:Allow users to search by date range, type, or amount. - Daterange - Done , Amount -DOne Type - Not Done
    FR 7.3Enable users to download statements in formats like PDF or CSV. - Done
    FR 7.4Provide charts or summaries for spending analysis. -Graph and Chart Done
    
    - Added few lines to Program.cs para gumana yung export to pdf
    
    -Added packages iTexttsharp.5.5.13.3
    
    Sample data set palang naka pasok dito di ko pa na implement yung database natin

--- Personal Expense Tracker/SQLQuery1.sql
+++ Personal Expense Tracker/SQLQuery1.sql
@@ -1,1 +1,8 @@
-﻿SELECT * FROM ExpenseUser
+﻿CREATE TABLE ExpenseUser(id INT PRIMARY KEY IDENTITY(1,1),
+username VARCHAR (99) NULL,
+password VARCHAR (99) NULL,
+date_create DATE NULL 
+);
+
+SELECT * FROM ExpenseUser
+
